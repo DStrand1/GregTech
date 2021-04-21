57f46790a4dbc81e833de2d0c7a9dccd214cf5a8
DStrand1 <dane@strandboge.com>
2021-04-20 20:50:28 -0500

    Merge GARecipeMaps, RecipeMapAssemblyLine
    Assembly Line needs some work still

--- src/main/java/gregtech/api/recipes/RecipeMaps.java
+++ src/main/java/gregtech/api/recipes/RecipeMaps.java
@@ -688,6 +688,43 @@ public class RecipeMaps {
 
 
     @ZenProperty
+    public static final RecipeMap<SimpleRecipeBuilder> CLUSTER_MILL_RECIPES = new RecipeMap<>("cluster_mill", 1, 1, 1, 1, 0, 0, 0, 0, new SimpleRecipeBuilder())
+            .setSlotOverlay(false, false, GuiTextures.BENDER_OVERLAY)
+            .setProgressBar(GuiTextures.PROGRESS_BAR_BENDING, MoveType.HORIZONTAL);
+
+
+    @ZenProperty
+    public static final RecipeMapAssemblyLine<SimpleRecipeBuilder> ASSEMBLY_LINE_RECIPES = new RecipeMapAssemblyLine<>("assembly_line", 4, 16, 1, 1, 0, 4, 0, 0, new SimpleRecipeBuilder())
+            .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, MoveType.HORIZONTAL);
+
+    // TODO This is pretty terrible
+    static {
+        ASSEMBLY_LINE_RECIPES.setSlotOverlay(false, false, GuiTextures.MOLD_OVERLAY);
+    }
+
+
+
+    @ZenProperty
+    public static final RecipeMap<SimpleRecipeBuilder> MASS_FAB_RECIPES = new RecipeMap<>("mass_fab", 0, 1, 0, 0, 0, 1, 1, 2, new SimpleRecipeBuilder())
+            .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, MoveType.HORIZONTAL);
+
+
+    @ZenProperty
+    public static final RecipeMap<SimpleRecipeBuilder> REPLICATOR_RECIPES = new RecipeMap<>("replicator", 0, 1, 0, 1, 1, 2, 0, 1, new SimpleRecipeBuilder())
+            .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, MoveType.HORIZONTAL);
+
+
+    @ZenProperty
+    public static final RecipeMap<SimpleRecipeBuilder> PROCESSING_ARRAY_RECIPES = new RecipeMap<>("processing_array", 0, 9, 0, 6, 0, 3, 0, 2, new SimpleRecipeBuilder())
+            .setProgressBar(GuiTextures.PROGRESS_BAR_BENDING, MoveType.HORIZONTAL);
+
+
+    @ZenProperty
+    public static final RecipeMap<IntCircuitRecipeBuilder> BUNDLER_RECIPES = new RecipeMap<>("bundler", 2, 2, 1, 1, 0, 0, 0, 0, new IntCircuitRecipeBuilder().EUt(12).duration(250))
+            .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, MoveType.HORIZONTAL);
+
+
+    @ZenProperty
     public static final FuelRecipeMap DIESEL_GENERATOR_FUELS = new FuelRecipeMap("diesel_generator");
 
 
@@ -703,6 +740,9 @@ public class RecipeMaps {
 
     @ZenProperty
     public static final FuelRecipeMap PLASMA_GENERATOR_FUELS = new FuelRecipeMap("plasma_generator");
+
+    @ZenProperty
+    public static final FuelRecipeMap NAQUADAH_REACTOR_FUELS = new FuelRecipeMap("naquadah_reactor");
 
     @ZenProperty
     public static final List<PrimitiveBlastFurnaceRecipe> PRIMITIVE_BLAST_FURNACE_RECIPES = new CopyOnWriteArrayList<>();
